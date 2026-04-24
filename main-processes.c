49a4b567aa2dbc0f7204c305d7ff2b3d40406089
Jayesh Manani <jmanani@3-B-5.42heilbronn.de>
2026-04-24 14:54:23 +0200

    learn processes

--- main-processes.c
+++ main-processes.c
@@ -1,39 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main-processes.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/24 14:24:32 by jmanani           #+#    #+#             */
-/*   Updated: 2026/04/24 14:54:07 by jmanani          ###   ########.fr       */
+/*   Created: 2026/04/24 14:46:25 by jmanani           #+#    #+#             */
+/*   Updated: 2026/04/24 14:53:43 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <sys/wait.h>
 #include <unistd.h>
-
-void	*routine(void)
-{
-	printf("Calling the routine\n");
-	sleep(2);
-	printf("End of Routine");
-}
 
 int	main(void)
 {
-	pthread_t	t1;
-	pthread_t	t2;
+	pid_t	pid1;
+	pid_t	pid2;
 
-	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
-		return (1);
-	if (pthread_create(&t2, NULL, &routine, NULL) != 0)
-		return (2);
-	if (pthread_join(t1, NULL) != 0)
-		return (3);
-	if (pthread_join(t2, NULL) != 0)
-		return (4);
+	pid1 = fork();
+	if (pid1 == 0)
+	{
+		printf("Child 1 PID: %d\n", getpid());
+		return (0);
+	}
+	pid2 = fork();
+	if (pid2 == 0)
+	{
+		printf("Child 2 PID: %d\n", getpid());
+		return (0);
+	}
+	// Only parent reaches here
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
+	printf("Parent PID: %d\n", getpid());
 	return (0);
 }
