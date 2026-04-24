cb85f8571b0df0aa46e1c8c16f3d8c5d31e163d4
Jayesh Manani <jmanani@3-B-5.42heilbronn.de>
2026-04-24 14:40:27 +0200

    threads learning

---
+++ main.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmanani <jmanani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/24 14:24:32 by jmanani           #+#    #+#             */
+/*   Updated: 2026/04/24 14:40:01 by jmanani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void	*routine(void)
+{
+	printf("Calling the routine\n");
+	sleep(2);
+	printf("End of Routine");
+}
+
+int	main(void)
+{
+	pthread_t	t1;
+	pthread_t	t2;
+
+	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
+		return (1);
+	if (pthread_create(&t2, NULL, &routine, NULL) != 0)
+		return (2);
+	if (pthread_join(t1, NULL) != 0)
+		return (3);
+	if (pthread_join(t2, NULL) != 0)
+		return (4);
+	return (0);
+}
