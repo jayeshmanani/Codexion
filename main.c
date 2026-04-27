896c3d3e191f4b3594a70ea85fb0aab148565872
Jayesh Manani <jmanani@3-D-6.42heilbronn.de>
2026-04-27 13:53:53 +0200

    return value from func

--- main.c
+++ main.c
@@ -13,24 +13,56 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
 
-int				g_mails = 0;
-pthread_mutex_t	g_mutex;
-
-void	*routine(void)
+void	*roll_dice(void)
 {
-	int	i;
+	int	value;
+	int	*result;
 
-	i = 0;
-	while (i < 10000000)
-	{
-		pthread_mutex_lock(&g_mutex);
-		g_mails++;
-		pthread_mutex_unlock(&g_mutex);
-		i++;
-	}
+	result = malloc(sizeof(int));
+	value = (rand() % 6) + 1;
+	*result = value;
+	printf("%d\n", *result);
+	return ((void *)result);
 }
+
+int	main(int argc, char *argv)
+{
+	int			*res;
+	pthread_t	th;
+
+	srand(time(NULL));
+	if (pthread_create(&th, NULL, &roll_dice, NULL) != 0)
+	{
+		return (1);
+	}
+	if (pthread_join(th, (void **)&res) != 0)
+	{
+		return (2);
+	}
+	printf("Result: %d\n", *res);
+	free(res);
+	return (0);
+}
+
+// int				g_mails = 0;
+// pthread_mutex_t	g_mutex;
+
+// void	*routine(void)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while (i < 10000000)
+// 	{
+// 		pthread_mutex_lock(&g_mutex);
+// 		g_mails++;
+// 		pthread_mutex_unlock(&g_mutex);
+// 		i++;
+// 	}
+// }
 
 // void	*routine2(void)
 // {
@@ -38,35 +70,35 @@ void	*routine(void)
 // 	printf("R2 Value of x: %d\n", x);
 // }
 
-int	main(void)
-{
-	pthread_t	th[4];
-	int			i;
-	int			j;
-	int			k;
+// int	main(void)
+// {
+// 	pthread_t	th[4];
+// 	int			i;
+// 	int			j;
+// 	int			k;
 
-	k = 8;
-	i = 0;
-	j = 0;
-	pthread_mutex_init(&g_mutex, NULL);
-	while (i < k)
-	{
-		if (pthread_create(th + i, NULL, &routine, NULL) != 0)
-		{
-			printf("Failed to create thread");
-			return (1);
-		}
-		printf("Thread %d is started\n", i);
-		i++;
-	}
-	while (j < k)
-	{
-		if (pthread_join(th[j], NULL) != 0)
-			return (4);
-		printf("Thread %d is finished\n", j);
-		j++;
-	}
-	pthread_mutex_destroy(&g_mutex);
-	printf("No. of mails: %d\n", g_mails);
-	return (0);
-}
+// 	k = 8;
+// 	i = 0;
+// 	j = 0;
+// 	pthread_mutex_init(&g_mutex, NULL);
+// 	while (i < k)
+// 	{
+// 		if (pthread_create(th + i, NULL, &routine, NULL) != 0)
+// 		{
+// 			printf("Failed to create thread");
+// 			return (1);
+// 		}
+// 		printf("Thread %d is started\n", i);
+// 		i++;
+// 	}
+// 	while (j < k)
+// 	{
+// 		if (pthread_join(th[j], NULL) != 0)
+// 			return (4);
+// 		printf("Thread %d is finished\n", j);
+// 		j++;
+// 	}
+// 	pthread_mutex_destroy(&g_mutex);
+// 	printf("No. of mails: %d\n", g_mails);
+// 	return (0);
+// }
