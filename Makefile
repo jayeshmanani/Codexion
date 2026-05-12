8e3171522267e5baca20a05d319dc16e38085368
Jayesh Manani <jmanani@3-C-9.42heilbronn.de>
2026-05-12 14:27:43 +0200

    added struct coder, dongle, scheduler enum

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC = gcc
-CFLAGS = -Wall -Wextra -Werror
+CFLAGS = -Wall -Wextra -Werror -pthread -Iinclude
 RM = rm -f
 
 NAME = codexion
--- include/codexion.h
+++ include/codexion.h
@@ -6,7 +6,7 @@
 /*   By: jmanani <jmanani@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 14:11:48 by jmanani           #+#    #+#             */
-/*   Updated: 2026/05/12 14:13:24 by jmanani          ###   ########.fr       */
+/*   Updated: 2026/05/12 14:26:57 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,31 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
+# include <sys/time.h>
 # include <time.h>
 # include <unistd.h>
+
+typedef enum e_scheduler
+{
+	FIFO,
+	EDF
+}					t_scheduler;
+
+typedef struct s_dongle
+{
+	pthread_mutex_t	mutex;
+}					t_dongle;
+
+typedef struct s_coder
+{
+	int				id;
+	pthread_t		thread;
+	int				compile_count;
+	long			last_compile_start;
+
+	t_dongle		*left;
+	t_dongle		*right;
+
+}					t_coder;
 
 #endif
