6a0b2fbfe0f59e86a4799b5f451d4dbf9d6be408
Jayesh Manani <jmanani@3-C-9.42heilbronn.de>
2026-05-12 14:15:28 +0200

    remove include .h folder

--- src/codexion.h
+++ src/codexion.h
@@ -1,19 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   codexion.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmanani <jmanani@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/12 14:09:49 by jmanani           #+#    #+#             */
-/*   Updated: 2026/05/12 14:15:02 by jmanani          ###   ########.fr       */
+/*   Created: 2026/05/12 14:11:48 by jmanani           #+#    #+#             */
+/*   Updated: 2026/05/12 14:13:24 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "codexion.h"
+#ifndef CODEXION_H
+# define CODEXION_H
 
-int	main(void)
-{
-	printf("Hello, World!\n");
-	return (0);
-}
+# include <limits.h>
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <time.h>
+# include <unistd.h>
+
+#endif
