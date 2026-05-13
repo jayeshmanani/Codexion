ba08bd556df94edec974260fb4a3546a384e79b5
Jayesh Manani <jmanani@3-C-3.42heilbronn.de>
2026-05-13 16:48:27 +0200

    added mutex and malloc sage

--- include/codexion.h
+++ include/codexion.h
@@ -6,7 +6,7 @@
 /*   By: jmanani <jmanani@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 14:11:48 by jmanani           #+#    #+#             */
-/*   Updated: 2026/05/12 16:00:43 by jmanani          ###   ########.fr       */
+/*   Updated: 2026/05/13 16:36:54 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define CODEXION_H
 
 // Default Libs
+# include <errno.h>
 # include <limits.h>
 # include <pthread.h>
 # include <stdbool.h>
@@ -25,56 +26,77 @@
 # include <unistd.h>
 
 // Customized Structs
-typedef pthread_mutex_t	t_mtx;
+typedef pthread_mutex_t			t_mtx;
+typedef struct s_coding_data	t_coding_data;
 
-typedef struct s_args	t_args;
+typedef enum e_mutex_ops
+{
+	CREATE,
+	JOIN,
+	INIT,
+	LOCK,
+	UNLOCK,
+	DESTROY,
+}								t_mutex_ops;
+
+// typedef enum e_cond_ops
+// {
+// 	INIT,
+// 	WAIT,
+// 	TIMEDWAIT,
+// 	BROADCAST,
+// 	DESTROY
+// }								t_cond_ops;
 
 typedef enum e_scheduler
 {
 	FIFO,
 	EDF
-}						t_scheduler;
+}								t_scheduler;
 
 typedef struct s_dongle
 {
-	int					dongle_id;
-	t_mtx				mutex;
-}						t_dongle;
+	int							dongle_id;
+	t_mtx						mutex;
+}								t_dongle;
 
 typedef struct s_coder
 {
-	int					id;
-	int					compilation_done;
-	bool				program_done;
-	long				last_compile_start;
+	int							id;
+	int							compilation_done;
+	bool						program_done;
+	long						last_compile_start;
 
-	pthread_t			coder_thread_id;
-	t_dongle			*left_dongle;
-	t_dongle			*right_dongle;
+	pthread_t					coder_thread_id;
+	t_dongle					*left_dongle;
+	t_dongle					*right_dongle;
 
-	t_args				*args;
-}						t_coder;
+	t_coding_data				*args;
+}								t_coder;
 
 typedef struct s_coding_data
 {
-	long				n_coders;
-	long				burn_time;
-	long				compile_time;
-	long				debug_time;
-	long				refactor_time;
-	long				n_compiles;
-	long				cooldown_time;
-	t_scheduler			scheduler;
+	long						n_coders;
+	long						burn_time;
+	long						compile_time;
+	long						debug_time;
+	long						refactor_time;
+	long						n_compiles;
+	long						cooldown_time;
+	t_scheduler					scheduler;
 
-	long				start_simulation;
-	bool				end_simulation;
+	long						start_simulation;
+	bool						end_simulation;
 
-	t_coder				*coders;
-	t_dongle			*dongles;
-}						t_coding_data;
+	t_coder						*coders;
+	t_dongle					*dongles;
+}								t_coding_data;
 
 // Other Prototypes from different files
-void					error_exit(const char *error);
-void					parse_input(t_args *coding_data, char **argv);
+void							error_exit(const char *error);
+void							parse_input(t_coding_data *coding_data,
+									char **argv);
+void							*malloc_safe_create(size_t bytes);
+void							*mutex_safe(t_mtx *mutex, t_mutex_ops ops);
 
 #endif
--- src/safe.c
+++ src/safe.c
@@ -1,20 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   initialize.c                                       :+:      :+:    :+:   */
+/*   safe.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmanani <jmanani@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/12 15:55:33 by jmanani           #+#    #+#             */
-/*   Updated: 2026/05/13 16:27:07 by jmanani          ###   ########.fr       */
+/*   Created: 2026/05/13 16:38:53 by jmanani           #+#    #+#             */
+/*   Updated: 2026/05/13 16:39:01 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "codexion.h"
-
-void	data_init(t_coding_data *cd)
-{
-	cd->end_simulation = false;
-	cd->coders = malloc_safe_create(cd->n_coders);
-	return ;
-}
