6c44dad19d17e7d55ab36ce1f5e83c5513940a5c
Jayesh Manani <jmanani@3-C-5.42heilbronn.de>
2026-05-23 21:48:58 +0200

    code restruct

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ NAME = codexion
 
 SRCS = src/main.c src/parsing.c src/initialize.c src/safe.c\
 		src/coding.c src/set_get.c src/sync.c src/data_op.c src/analyser.c\
-		src/dongle_sync.c src/coding_utils.c src/safe_cond.c \
+		src/coding_utils.c src/safe_cond.c \
 		src/end_condition.c  src/global_queue.c  src/global_queue_utils.c \
 		src/heap/heap_utils.c src/heap/heap_ops.c src/heap/heap_cmp.c\
 		src/heap/global_init.c\
--- src/utils/dongle_util.c
+++ src/utils/dongle_util.c
@@ -6,7 +6,7 @@
 /*   By: jmanani <jmanani@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/17 14:37:54 by jmanani           #+#    #+#             */
-/*   Updated: 2026/05/23 21:44:47 by jmanani          ###   ########.fr       */
+/*   Updated: 2026/05/23 21:47:46 by jmanani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,4 +101,28 @@ int	wait_acquire_both_dongles(t_coder *coder)
 	heap_remove(coder->cd->global_heap, coder->coder_id);
 	mutex_safe(&coder->cd->global_mutex, UNLOCK);
 	return (1);
+}
+
+int	release_dongle(t_coder *coder, t_dongle *dongle)
+{
+	if (!coder || !dongle)
+		return (1);
+	if (mutex_safe(&dongle->dongle_mutex, LOCK) != 0)
+		return (1);
+	dongle->is_taken = false;
+	dongle->next_available_t = get_time(MILLISEC) + coder->cd->cooldown_time;
+	if (cond_safe(&dongle->dongle_cond, NULL, BROADCAST, NULL) != 0)
+	{
+		mutex_safe(&dongle->dongle_mutex, UNLOCK);
+		return (1);
+	}
+	if (mutex_safe(&dongle->dongle_mutex, UNLOCK) != 0)
+		return (1);
+	if (coder->cd->global_cond_initialized)
+	{
+		mutex_safe(&coder->cd->global_mutex, LOCK);
+		cond_safe(&coder->cd->global_cond, NULL, BROADCAST, NULL);
+		mutex_safe(&coder->cd->global_mutex, UNLOCK);
+	}
+	return (0);
 }
